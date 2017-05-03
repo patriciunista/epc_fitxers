aa70e04f323eec8aee770dc325aab9fd5a3b1ba7
Patriciu Nista <subgeo98@gmail.com>
2017-05-04 01:36:06 +0200

    Afegir info estat del exercici

--- Exercici 3/exercici3.c
+++ Exercici 3/exercici3.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "funcions.h"
 
+/*
+	FALTA:
+		-- Validacio al introduir dades. (Si han de ser numeros numeros)
+
+	EXTRES:
+		-- Afegir opcio eliminar registre dient num matricula. (De cualsevol dels fitxers)
+		-- Afegir comprovacio per no introduir dades redundants. (Clau primaria = num matricula)
+		-- Mes colors
+*/
 
 int main()
 {
--- Exercici 3/funcions.h
+++ Exercici 3/funcions.h
@@ -9,11 +9,11 @@
 #define FICH_MOVI_FILE "registres/FICH_MOVI.dat" // ordenat per el mv_nmat
 #define FICH_ALUM_FILE "registres/FICH_ALUM.dat" // ordenat per el alu_nmat
 #define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
+//#define ANSI_COLOR_GREEN   "\x1b[32m"
+//#define ANSI_COLOR_YELLOW  "\x1b[33m"
+//#define ANSI_COLOR_BLUE    "\x1b[34m"
+//#define ANSI_COLOR_MAGENTA "\x1b[35m"
+//#define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
 typedef struct // Definicio del registre fitxer ANT_ALUM
